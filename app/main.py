a9ed01c31f827a169a3ad38c743299aa924233a3
Ilja Gubins <ilja.gubin@gmail.com>
2022-03-29 02:21:09 +0200

    Sending email in thread

--- app/main.py
+++ app/main.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 
 @app.route("/mail-webhook", methods=["POST"])
 def mail_webhook():
+    print(request.form.to_dict())
     email_subject = request.form.get("subject", "[unknown]")
     email_sender = request.form.get("from", request.form.get("sender", "[unknown]"))
     if " <" in email_sender:
@@ -18,14 +19,29 @@ def mail_webhook():
     sender_hash = hashlib.sha1(email_sender.encode("utf-8")).hexdigest()
     avatar = "https://www.gravatar.com/avatar/%s?d=retro" % sender_hash
 
-    resp = requests.post(
+    # Announcement
+    announcement = requests.post(
         url="https://slack.com/api/chat.postMessage",
         data={
             "token": SLACK_API_TOKEN,
             "channel": SLACK_CHANNEL,
             "icon_url": avatar,
             "parse": "full",
-            "text": email_subject,
+            "text": f"Subject: {email_subject}",
+            "username": email_sender,
+        },
+    )
+
+    # Actual email content
+    content = requests.post(
+        url="https://slack.com/api/chat.postMessage",
+        data={
+            "token": SLACK_API_TOKEN,
+            "channel": SLACK_CHANNEL,
+            "thread_ts": announcement.json()["ts"],
+            "icon_url": avatar,
+            "parse": "full",
+            "text": f"{email_subject}",
             "username": email_sender,
         },
     )
