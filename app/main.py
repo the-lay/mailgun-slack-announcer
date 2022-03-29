e5d00b243cd57d5688d81ec50af263f266d31ef7
Ilja Gubins <ilja.gubin@gmail.com>
2022-03-29 02:48:21 +0200

    More formatting and attachments support

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import os
-import hashlib
 import requests
+from datetime import datetime
+
 from flask import Flask, request
 
 SLACK_API_TOKEN = os.environ.get("SLACK_API_TOKEN")
@@ -11,24 +12,28 @@ app = Flask(__name__)
 
 @app.route("/mail-webhook", methods=["POST"])
 def mail_webhook():
-    print(request.form.to_dict())
-    email_subject = request.form.get("subject", "[unknown]")
 
-    # Announcement
+    sender = request.form.get("from", request.form.get("sender", "[unknown]"))
+    recipients = ", ".join(request.form.get("recipient", "[unknown]").split(","))
+    timestamp = int(request.form.get("timestamp", 0))
+    parsed_timestamp = datetime.fromtimestamp(timestamp)
+    subject = request.form.get("subject", "[unknown]")
+    plain_text = request.form.get("body-plain", "[unknown]")
+    n_attachments = int(request.form.get("attachment-count", 0))
+
     announcement = requests.post(
         url="https://slack.com/api/chat.postMessage",
         data={
             "token": SLACK_API_TOKEN,
             "channel": SLACK_CHANNEL,
             "parse": "full",
-            "text": f"Sender: {request.form.get('from', request.form.get('sender', '[unknown]'))}\n"
-            f"To: {request.form.get('recipient', '[unknown]')}\n"
-            f"Time: {request.form.get('recipient', '[unknown]')}\n"
-            f"Subject: {request.form.get('subject', '[unknown]')}",
+            "text": f"Sender: {sender}\n"
+            f"Recipients: {recipients}\n"
+            f"Time: {parsed_timestamp:%Y-%m-%d %H:%M:%S}\n"
+            f"Subject: {subject}",
         },
     )
 
-    # Actual email content
     content = requests.post(
         url="https://slack.com/api/chat.postMessage",
         data={
@@ -36,8 +41,22 @@ def mail_webhook():
             "channel": SLACK_CHANNEL,
             "thread_ts": announcement.json()["ts"],
             "parse": "full",
-            "text": f"{request.form.get('body-plain', '[unknown]')}",
+            "text": f"{plain_text}",
         },
     )
+
+    for i in range(n_attachments):
+        attachment = request.form.get(f"attachment-{i}", "[unknown]")
+
+        requests.post(
+            url="https://slack.com/api/files.upload",
+            data={
+                "token": SLACK_API_TOKEN,
+                "channel": SLACK_CHANNEL,
+                "thread_ts": announcement.json()["ts"],
+                "parse": "full",
+                "file": attachment,
+            },
+        )
 
     return "OK"
